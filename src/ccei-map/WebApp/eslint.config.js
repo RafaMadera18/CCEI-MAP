7660ee0aec0a21972f0d72c58af528f6fb67d698
RaptorRush135 <112044769+RaptorRush135@users.noreply.github.com>
2025-09-28 21:13:40 -0600

    feat: Add empty project

---
+++ src/ccei-map/WebApp/eslint.config.js
@@ -0,0 +1,58 @@
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import angular from 'angular-eslint';
+
+export default tseslint.config([
+  {
+    files: ['**/*.ts'],
+    extends: [
+      eslint.configs.recommended,
+      ...tseslint.configs.recommended,
+      ...tseslint.configs.stylistic,
+      ...angular.configs.tsRecommended,
+    ],
+    processor: angular.processInlineTemplates,
+    rules: {
+      '@angular-eslint/component-selector': [
+        'error',
+        {
+          type: 'element',
+          prefix: 'app',
+          style: 'kebab-case',
+        },
+      ],
+      '@angular-eslint/directive-selector': [
+        'error',
+        {
+          type: 'attribute',
+          prefix: 'app',
+          style: 'camelCase',
+        },
+      ],
+      '@typescript-eslint/no-restricted-imports': [
+        'error',
+        {
+          patterns: [
+            {
+              group: ['**/../**'],
+              message: 'Backwards relative imports should not be used. Use path aliases instead.',
+            },
+          ],
+          paths: [
+            {
+              name: '@angular/core',
+              importNames: ['Input'],
+              message:
+                "'Input' or 'Output' decorator from '@angular/core' should not be used. Use 'input' or 'output' signal instead.",
+            },
+          ],
+        },
+      ],
+    },
+  },
+  {
+    files: ['**/*.html'],
+    extends: [...angular.configs.templateRecommended, ...angular.configs.templateAccessibility],
+    rules: {},
+  },
+]);
