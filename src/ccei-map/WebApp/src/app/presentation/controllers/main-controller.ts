3f7a8ace52cb658b9c3793a28138b1e977007fe5
soy-russ-bp <russelbonilla9@gmail.com>
2025-12-11 12:57:27 -0600

    added shortest path dependency injection

--- src/ccei-map/WebApp/src/app/presentation/controllers/main-controller.ts
+++ src/ccei-map/WebApp/src/app/presentation/controllers/main-controller.ts
@@ -1,11 +1,13 @@
 import { Space } from "@domain/entities";
 import { inject, Injectable } from "@angular/core";
 import { GET_ALL_SPACES_USE_CASE } from "@di/dependencies-provider";
+import { CALCULATE_SHORTEST_PATH_USE_CASE } from "@di/dependencies-provider";
 
 @Injectable({providedIn: 'root'})
 export class MainController {
 
   private readonly getAllSpacesUseCase = inject(GET_ALL_SPACES_USE_CASE);
+  private readonly calculateShortestPathUseCase = inject(CALCULATE_SHORTEST_PATH_USE_CASE);
 
   getAllSpaces(): Space[] {
     return this.getAllSpacesUseCase.execute();
