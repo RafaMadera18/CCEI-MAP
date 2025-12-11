15727c1e0da3ac438086735fbffec8e91ce5b338
Gerardo Villanueva <A22216369@alumnos.uady.mx>
2025-12-10 22:12:02 -0600

    refactor: update Space component tests to use SpacePage and mock controller

--- src/ccei-map/WebApp/src/app/presentation/pages/space/space.spec.ts
+++ src/ccei-map/WebApp/src/app/presentation/pages/space/space.spec.ts
@@ -1,18 +1,31 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideRouter } from '@angular/router';
+import { provideZonelessChangeDetection } from '@angular/core';
+import { SpacesController } from '@controllers/spaces-controller';
+import { SpacePage } from './space';
 
-import { Space } from './space';
+class MockSpacesController {
+  getSpaceById() {
+    return null;
+  }
+}
 
-describe('Space', () => {
-  let component: Space;
-  let fixture: ComponentFixture<Space>;
+describe('SpacePage', () => {
+  let component: SpacePage;
+  let fixture: ComponentFixture<SpacePage>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [Space]
+      imports: [SpacePage],
+      providers: [
+        provideZonelessChangeDetection(),
+        provideRouter([]),
+        { provide: SpacesController, useClass: MockSpacesController }
+      ]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(Space);
+    fixture = TestBed.createComponent(SpacePage);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
